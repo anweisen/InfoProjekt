226e12972b20f6540ff511d956b7a3ca278520c7
LinaWu26 <lina.wu@gmg.amberg.de>
2025-05-21 14:00:17 +0200

    Enemy Klasse erstellt

---
+++ src/Enemy.java
@@ -0,0 +1,15 @@
+public class Enemy implements Enemy_Abstract{
+    
+    String name;
+    int health;
+    int power;
+
+
+    public Enemy (String n, int h, int p){
+        name = n;
+        health = h;
+        power = p;
+
+    }
+
+}
