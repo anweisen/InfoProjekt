fdd170c380dfc680901731de54480d0d6419a15e
Felix S <felix@scharf.am>
2025-05-22 13:09:25 +1000

    setter und getter
    setter und getter für Tower geschrieben

--- src/tower/AbstractTower.java
+++ src/tower/AbstractTower.java
@@ -1,9 +1,18 @@
 package tower;
 
 public abstract class AbstractTower {
+
+    double x, y; // Position auf dem Spielfeld (2D-Koordinaten)
+    double range; // Reichweite des Turms
+    double damage; // Schaden pro Schuss
+    double fireRate; // Zeit zwischen Schüssen (z.B. in Millisekunden oder Ticks)
+    long lastShotTime; // Zeitstempel des letzten Schusses
+    String projectileType; // Art des Projektils ("Pfeil", "Feuerball", etc.)
+    double cost; // Baukosten
+    double level; // Upgrade-Level
+
     public abstract double getX();
 
     public abstract double getY();
 
-    public abstract double getImage();
 }
